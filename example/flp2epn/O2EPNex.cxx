3d0a1062a54e6213291e91eda12a6526e9894826
chkouzin <charalampos.kouzinopoulos@cern.ch>
2014-08-13 15:24:14 +0200

    Changed the FLP and EPN code from multi-threaded to single-threaded

--- example/flp2epn/O2EPNex.cxx
+++ example/flp2epn/O2EPNex.cxx
@@ -15,58 +15,51 @@ O2EPNex::O2EPNex()
 {
 }
 
-void O2EPNex::ReceivePayload()
-{
-  while ( fState == RUNNING ) {
-    FairMQMessage* msg = fTransportFactory->CreateMessage();
-
-    fPayloadInputs->at(0)->Receive(msg);
-    
-    //LOG(INFO) << "Received payload from FLP";
-
-    int inputSize = msg->GetSize();
-    int numInput = inputSize / sizeof(Content);
-    Content* input = reinterpret_cast<Content*>(msg->GetData());
-
-    // for (int i = 0; i < numInput; ++i) {
-    //     LOG(INFO) << (&input[i])->x << " " << (&input[i])->y << " " << (&input[i])->z << " " << (&input[i])->a << " " << (&input[i])->b;
-    // }
-
-    delete msg;
-  }
-}
-
-void O2EPNex::SendHeartbeat()
-{
-  while ( fState == RUNNING ) {
-    
-    for (int iOutput = 0; iOutput < fNumOutputs; iOutput++) {
-      FairMQMessage* msg = fTransportFactory->CreateMessage (strlen (fInputAddress.at(0).c_str()));
-      memcpy(msg->GetData(), fInputAddress.at(0).c_str(), strlen (fInputAddress.at(0).c_str()));
-    
-      fPayloadOutputs->at(iOutput)->Send(msg);
-      
-      delete msg;
-    }
-    
-    //LOG(INFO) << "Publishing identification string";
-    
-    boost::this_thread::sleep(boost::posix_time::milliseconds(fHeartbeatIntervalInMs));
-  }
-}
-
 void O2EPNex::Run()
 {
   LOG(INFO) << ">>>>>>> Run <<<<<<<";
 
   boost::thread rateLogger(boost::bind(&FairMQDevice::LogSocketRates, this));
-
-  boost::thread fReceiveThread (boost::bind(&O2EPNex::ReceivePayload, this));
-  boost::thread fSendThread (boost::bind(&O2EPNex::SendHeartbeat, this));
     
-  //Wait for the threads to finish execution
-  fReceiveThread.join();
-  fSendThread.join();
+  boost::posix_time::ptime referenceTime = boost::posix_time::microsec_clock::local_time();
+  
+  //Set the time difference to fHeartbeatIntervalInMs to immediately send a heartbeat to the EPNs
+  int timeDif = fHeartbeatIntervalInMs;
+  
+  while ( fState == RUNNING ) {
+    if ( timeDif >= fHeartbeatIntervalInMs) {
+      referenceTime = boost::posix_time::microsec_clock::local_time();
+
+      for (int iOutput = 0; iOutput < fNumOutputs; iOutput++) {
+        FairMQMessage* heartbeatMsg = fTransportFactory->CreateMessage (strlen (fInputAddress.at(0).c_str()));
+        memcpy(heartbeatMsg->GetData(), fInputAddress.at(0).c_str(), strlen (fInputAddress.at(0).c_str()));
+      
+        fPayloadOutputs->at(iOutput)->Send(heartbeatMsg);
+        
+        delete heartbeatMsg;
+      }
+    }
+    
+    //Update the time difference
+    timeDif = (boost::posix_time::microsec_clock::local_time() - referenceTime).total_milliseconds();
+
+    //Receive payload
+    FairMQMessage* payloadMsg = fTransportFactory->CreateMessage();
+
+    size_t payloadSize = fPayloadInputs->at(0)->Receive(payloadMsg, "no-block");
+    
+    if ( payloadSize > 0 ) {
+      int inputSize = payloadMsg->GetSize();
+      int numInput = inputSize / sizeof(Content);
+      Content* input = reinterpret_cast<Content*>(payloadMsg->GetData());
+  
+      // for (int i = 0; i < numInput; ++i) {
+      //     LOG(INFO) << (&input[i])->x << " " << (&input[i])->y << " " << (&input[i])->z << " " << (&input[i])->a << " " << (&input[i])->b;
+      // }
+    }
+
+    delete payloadMsg;
+  }
 
   rateLogger.interrupt();
   rateLogger.join();
